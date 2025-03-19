65958c89c69e60b4f15251474da488a611d79c3b
Konstantin Belousov <kib@FreeBSD.org>
2025-03-20 01:39:01 +0200

    Figure out errno/strerror

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::process;
+use std::ptr;
+use std::io::Error;
+use std::ffi::CStr;
+use std::ffi::c_char;
+use libc;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -11,7 +17,42 @@ struct PollArgs {
     #[arg(short, long, action = clap::ArgAction::Count)]
     verbose: u8,
 }
+
+fn c_char_ptr_to_string(ptr: *const c_char) -> String {
+    unsafe {
+	CStr::from_ptr(ptr).to_str().unwrap().to_string()
+    }
+}
+
+fn strerror(errno:i32) -> String {
+    unsafe {
+	c_char_ptr_to_string(libc::strerror(errno))
+    }
+}
+
+fn get_errno() -> libc::c_int {
+    Error::last_os_error().raw_os_error().unwrap()
+}
+
 fn main() {
     let args = PollArgs::parse();
     println!("args id {} verbose {:?}", args.id, args.verbose);
+
+    let res = unsafe {
+	libc::ptrace(libc::PT_ATTACH, args.id as i32, ptr::null_mut(), 0)
+    };
+    if res == -1 {
+	let errno = get_errno();
+	eprintln!("Attach to {} failed: {}", args.id, strerror(errno));
+	process::exit(1);
+    }
+    let res = unsafe {
+	libc::ptrace(libc::PT_DETACH, args.id as i32, ptr::null_mut(), 0)
+    };
+    if res == -1 {
+	let errno = get_errno();
+	eprintln!("Detach from {} failed: {}", args.id, strerror(errno));
+	process::exit(1);
+    }
+    process::exit(0);
 }
