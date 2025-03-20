bce25dc0adfc2f1310c112cf7c5c80c81b265aae
Konstantin Belousov <kib@FreeBSD.org>
2025-03-20 06:20:44 +0200

    Move poll handler into the dedicated function

--- src/main.rs
+++ src/main.rs
@@ -34,6 +34,55 @@ fn get_errno() -> libc::c_int {
     Error::last_os_error().raw_os_error().unwrap()
 }
 
+fn handle_poll(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
+    let nargs = lwpi.pl_syscall_narg as usize;
+    let mut scargs = Vec::<libc::register_t>::with_capacity(nargs);
+    scargs.resize(nargs, 0);
+    let scargs_raw = scargs.as_mut_ptr();
+    let res = unsafe {
+	libc::ptrace(libc::PT_GET_SC_ARGS, args.id as i32,
+		     scargs_raw as *mut i8, 0)
+    };
+    if res == -1 {
+	let errno = get_errno();
+	eprintln!("Fetching poll args failed: {}", strerror(errno));
+	process::exit(1);
+    }
+    if args.verbose >= 2 {
+	eprintln!("Fetched poll args pfds[] {} nfds {}",
+		  scargs[0], scargs[1]);
+    }
+
+    let nfds = scargs[1] as usize;
+    let mut pfds = Vec::<libc::pollfd>::with_capacity(nfds);
+    pfds.resize(nfds, libc::pollfd { fd: 0, events: 0, revents: 0 });
+    let pfds_raw = pfds.as_mut_ptr();
+    let mut pt_io_desc = libc::ptrace_io_desc {
+	piod_op: libc::PIOD_READ_D,
+	piod_offs: unsafe { std::mem::transmute(scargs[0]) },
+	piod_addr: pfds_raw as *mut libc::c_void,
+	piod_len: nfds * std::mem::size_of::<libc::pollfd>(),
+    };
+    let res = unsafe {
+	libc::ptrace(libc::PT_IO, args.id as i32,
+		     &raw mut pt_io_desc as *mut i8, 0)
+    };
+    if res == -1 {
+	let errno = get_errno();
+	eprintln!("Fetching pollfd array failed: {}", strerror(errno));
+	process::exit(1);
+    }
+    if args.verbose >= 2 {
+	eprintln!("Fetched pollfd array");
+    }
+
+    println!("lwp id {} polling on:", lwpi.pl_lwpid);
+    pfds.iter().filter(|pfd| { pfd.fd >= 0 }).for_each({|pfd| {
+	println!("{}", pfd.fd);
+      }
+    });
+}
+
 fn main() {
     let args = PollArgs::parse();
 
@@ -85,52 +134,7 @@ fn main() {
 
     if lwpi.pl_syscall_code == 209 /* poll */ ||
 	lwpi.pl_syscall_code == 545 /* ppoll */ {
-	    let nargs = lwpi.pl_syscall_narg as usize;
-	    let mut scargs = Vec::<libc::register_t>::with_capacity(nargs);
-	    scargs.resize(nargs, 0);
-	    let scargs_raw = scargs.as_mut_ptr();
-	    let res = unsafe {
-		libc::ptrace(libc::PT_GET_SC_ARGS, args.id as i32,
-		    scargs_raw as *mut i8, 0)
-	    };
-	    if res == -1 {
-		let errno = get_errno();
-		eprintln!("Fetching poll args failed: {}", strerror(errno));
-		process::exit(1);
-	    }
-	    if args.verbose >= 2 {
-		eprintln!("Fetched poll args pfds[] {} nfds {}",
-			  scargs[0], scargs[1]);
-	    }
-
-	    let nfds = scargs[1] as usize;
-	    let mut pfds = Vec::<libc::pollfd>::with_capacity(nfds);
-	    pfds.resize(nfds, libc::pollfd { fd: 0, events: 0, revents: 0 });
-	    let pfds_raw = pfds.as_mut_ptr();
-	    let mut pt_io_desc = libc::ptrace_io_desc {
-		piod_op: libc::PIOD_READ_D,
-		piod_offs: unsafe { std::mem::transmute(scargs[0]) },
-		piod_addr: pfds_raw as *mut libc::c_void,
-		piod_len: nfds * std::mem::size_of::<libc::pollfd>(),
-	    };
-	    let res = unsafe {
-		libc::ptrace(libc::PT_IO, args.id as i32,
-		    &raw mut pt_io_desc as *mut i8, 0)
-	    };
-	    if res == -1 {
-		let errno = get_errno();
-		eprintln!("Fetching pollfd array failed: {}", strerror(errno));
-		process::exit(1);
-	    }
-	    if args.verbose >= 2 {
-		eprintln!("Fetched pollfd array");
-	    }
-
-	    println!("lwp id {} polling on:", lwpi.pl_lwpid);
-	    pfds.iter().filter(|pfd| { pfd.fd >= 0 }).for_each({|pfd| {
-		println!("{}", pfd.fd);
-	      }
-	    });
+	    handle_poll(&lwpi, &args)
     }
 
     let res = unsafe {
