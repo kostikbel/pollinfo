baa47dafbc46b348221037c0954e8de240da1f3c
Konstantin Belousov <kib@FreeBSD.org>
2025-03-21 07:15:34 +0200

    Add context and replace direct usage of the PollArgs with Context

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,12 @@ struct PollArgs {
     verbose: u8,
 }
 
+struct Context {
+    id: u32,
+    verbose: u8,
+    attached: bool,
+}
+
 fn c_char_ptr_to_string(ptr: *const c_char) -> String {
     unsafe { CStr::from_ptr(ptr).to_str().unwrap().to_string() }
 }
@@ -44,7 +50,7 @@ fn get_errno() -> libc::c_int {
 }
 
 macro_rules! call_ptrace {
-    ($ptrace_op:expr, $args:expr, $lwpid:expr, $addr:expr, $data:expr,
+    ($ptrace_op:expr, $ctx:expr, $lwpid:expr, $addr:expr, $data:expr,
      $err_fmt:expr, $ok_fmt:expr, $($x:expr,)* ) => {{
         let res = unsafe {
 	    libc::ptrace($ptrace_op, $lwpid as i32, $addr as *mut i8,
@@ -55,14 +61,14 @@ macro_rules! call_ptrace {
             eprintln!($err_fmt, strerror(errno));
             process::exit(1);
         }
-        if $args.verbose >= 2 {
+        if $ctx.verbose >= 2 {
             eprintln!($ok_fmt, $($x,)*);
         }
 	res
      }}
 }
 
-fn handle_poll(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
+fn handle_poll(lwpi: &libc::ptrace_lwpinfo, ctx: &Context) {
     let nargs = lwpi.pl_syscall_narg as usize;
     if nargs < 3 {
 	eprintln!("poll-like syscall no {} with {} args",
@@ -72,7 +78,7 @@ fn handle_poll(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
     let mut scargs = vec![0; nargs];
     let scargs_raw = scargs.as_mut_ptr();
     call_ptrace!(
-        libc::PT_GET_SC_ARGS, args, args.id, scargs_raw,
+        libc::PT_GET_SC_ARGS, ctx, lwpi.pl_lwpid, scargs_raw,
 	nargs * std::mem::size_of::<libc::register_t>(),
         "Fetching poll args failed: {}",
         "Fetched poll args pfds[] {} nfds {}", scargs[0], scargs[1],
@@ -89,7 +95,7 @@ fn handle_poll(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
             piod_len: nfds * std::mem::size_of::<libc::pollfd>(),
 	};
 	call_ptrace!(
-	    libc::PT_IO, args, args.id, &raw mut pt_io_desc, 0,
+	    libc::PT_IO, ctx, lwpi.pl_lwpid, &raw mut pt_io_desc, 0,
 	    "Fetching pollfd array failed: {}",
 	    "Fetched pollfd array",
 	);
@@ -106,7 +112,7 @@ fn handle_poll(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
     }});
 }
 
-fn handle_select_fetch_fds(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs,
+fn handle_select_fetch_fds(lwpi: &libc::ptrace_lwpinfo, ctx: &Context,
 fds: &mut Vec<u8>, fds_len: usize, off: libc::register_t, name: &str) {
     if off == 0 {
 	return;
@@ -119,13 +125,13 @@ fds: &mut Vec<u8>, fds_len: usize, off: libc::register_t, name: &str) {
         piod_len: fds_len,
     };
     call_ptrace!(
-	libc::PT_IO, args, lwpi.pl_lwpid, &raw mut pt_io_desc, 0,
+	libc::PT_IO, ctx, lwpi.pl_lwpid, &raw mut pt_io_desc, 0,
 	"Fetching fds array failed: {}",
 	"Fetched {}fds array", name,
     );
 }
 
-fn handle_select(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
+fn handle_select(lwpi: &libc::ptrace_lwpinfo, ctx: &Context) {
     let nargs = lwpi.pl_syscall_narg as usize;
     if nargs < 5 {
 	eprintln!("select-like syscall no {} with {} args",
@@ -135,7 +141,7 @@ fn handle_select(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
     let mut scargs = vec![0; nargs];
     let scargs_raw = scargs.as_mut_ptr();
     call_ptrace!(
-        libc::PT_GET_SC_ARGS, args, lwpi.pl_lwpid, scargs_raw,
+        libc::PT_GET_SC_ARGS, ctx, lwpi.pl_lwpid, scargs_raw,
 	nargs * std::mem::size_of::<libc::register_t>(),
         "Fetching select args failed: {}",
         "Fetched select args nfds {} {:#x} {:#x} {:#x}",
@@ -150,11 +156,11 @@ fn handle_select(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
     let fds_len = nfds.div_ceil(u8::BITS as usize);
 
     let mut infds = vec![0u8; fds_len];
-    handle_select_fetch_fds(lwpi, args, &mut infds, fds_len, scargs[1], "in");
+    handle_select_fetch_fds(lwpi, ctx, &mut infds, fds_len, scargs[1], "in");
     let mut outfds = vec![0u8; fds_len];
-    handle_select_fetch_fds(lwpi, args, &mut outfds, fds_len, scargs[2], "out");
+    handle_select_fetch_fds(lwpi, ctx, &mut outfds, fds_len, scargs[2], "out");
     let mut exfds = vec![0u8; fds_len];
-    handle_select_fetch_fds(lwpi, args, &mut exfds, fds_len, scargs[3], "ex");
+    handle_select_fetch_fds(lwpi, ctx, &mut exfds, fds_len, scargs[3], "ex");
 
     for fd in 0..(nfds - 1) {
 	let idx = fd / u8::BITS as usize;
@@ -169,10 +175,10 @@ fn handle_select(lwpi: &libc::ptrace_lwpinfo, args: &PollArgs) {
     }
 }
 
-fn handle_lwp(args: &PollArgs, lwpid: libc::lwpid_t) {
+fn handle_lwp(ctx: &Context, lwpid: libc::lwpid_t) {
     let mut lwpi: libc::ptrace_lwpinfo = unsafe { std::mem::zeroed() };
     call_ptrace!(
-        libc::PT_LWPINFO, args, lwpid, &raw mut lwpi,
+        libc::PT_LWPINFO, ctx, lwpid, &raw mut lwpi,
         std::mem::size_of::<libc::ptrace_lwpinfo>(),
         "Fetching lwpinfo failed: {}",
         "Fetched lwpinfo lwpid {} event {} flags {:#x} syscall {} nargs {}",
@@ -182,25 +188,30 @@ fn handle_lwp(args: &PollArgs, lwpid: libc::lwpid_t) {
 
     if lwpi.pl_syscall_code == 209 /* poll */ ||
 	lwpi.pl_syscall_code == 545 /* ppoll */ {
-            handle_poll(&lwpi, args)
+            handle_poll(&lwpi, ctx)
     } else if lwpi.pl_syscall_code == 93 /* select */ ||
 	lwpi.pl_syscall_code == 522 /* pselect */ {
-            handle_select(&lwpi, args)
+            handle_select(&lwpi, ctx)
     }
 }
 
 fn main() {
     let args = PollArgs::parse();
+    let mut ctx = Context {
+	id: args.id,
+	verbose: args.verbose,
+	attached: false,
+    };
 
     call_ptrace!(
-	libc::PT_ATTACH, args, args.id, ptr::null_mut(), 0,
+	libc::PT_ATTACH, &ctx, ctx.id, ptr::null_mut(), 0,
         "Attach failed: {}",
-        "Attached to {}", args.id,
+        "Attached to {}", ctx.id,
     );
 
     let mut si: libc::siginfo_t = unsafe { std::mem::zeroed() };
     let res = unsafe {
-	libc::waitid(libc::P_PID, args.id as libc::id_t,
+	libc::waitid(libc::P_PID, ctx.id as libc::id_t,
 		     &mut si, libc::WUNTRACED)
     };
     if res == -1 {
@@ -208,33 +219,33 @@ fn main() {
         eprintln!("Wait for initial stop failed: {}", strerror(errno));
         process::exit(1);
     }
-    if args.verbose >= 2 {
+    if ctx.verbose >= 2 {
         eprintln!("Consumed initial stop event");
     }
 
     let nlwps: usize = call_ptrace!(
-	libc::PT_GETNUMLWPS, args, args.id, ptr::null_mut(), 0,
+	libc::PT_GETNUMLWPS, &ctx, ctx.id, ptr::null_mut(), 0,
 	"Querying the number of lwps failed: {}",
-	"Fetched number of lwps for process {}", args.id,) as usize;
-    if args.verbose >= 2 {
-	eprintln!("Process {} executes {} lwps", args.id, nlwps);
+	"Fetched number of lwps for process {}", ctx.id,) as usize;
+    if ctx.verbose >= 2 {
+	eprintln!("Process {} executes {} lwps", ctx.id, nlwps);
     }
     let mut lwpids = vec![0; nlwps];
     let lwpids_raw = lwpids.as_mut_ptr();
     call_ptrace!(
-	libc::PT_GETLWPLIST, args, args.id, lwpids_raw,
+	libc::PT_GETLWPLIST, &ctx, ctx.id, lwpids_raw,
 	nlwps * std::mem::size_of::<libc::lwpid_t>(),
 	"Fetching list of lwpids failed: {}",
-	"Fetched list of lwpids for process {}", args.id,
+	"Fetched list of lwpids for process {}", ctx.id,
     );
     lwpids.iter().for_each(|lwpid| {
-	handle_lwp(&args, *lwpid)
+	handle_lwp(&ctx, *lwpid)
     });
 
     call_ptrace!(
-	libc::PT_DETACH, args, args.id, ptr::null_mut(), 0,
+	libc::PT_DETACH, &ctx, ctx.id, ptr::null_mut(), 0,
         "Detach failed: {}",
-        "Detached from {}", args.id,
+        "Detached from {}", ctx.id,
     );
     process::exit(0);
 }
